43babdf76b3c85be80bf5044c2fa21f18e2c9539
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-07-11 03:26:39 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -16,21 +16,21 @@ import {
     getStorage
 } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
 
-// Firebase Config
+// Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyByJ-da2vnaRH-ySL0QoLENkK0uTMSgyG8",
-    authDomain: "raxy-sms.firebaseapp.com",
-    projectId: "raxy-sms",
-    storageBucket: "raxy-sms.firebasestorage.app",
-    messagingSenderId: "801661467553",
-    appId: "1:801661467553:web:81eceac958bbbe1010a9e0",
-    measurementId: "G-BDD2M9S500"
+    apiKey: "AIzaSyAd2RYdabzYT86_PyMJZ6zxWQA5Vtgj3Bw",
+    authDomain: "raxy-sms-ea4c2.firebaseapp.com",
+    projectId: "raxy-sms-ea4c2",
+    storageBucket: "raxy-sms-ea4c2.firebasestorage.app",
+    messagingSenderId: "454128841738",
+    appId: "1:454128841738:web:5ccd2c9096cdfe2f435bf3",
+    measurementId: "G-JD4GY4YNCC"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Services
+// Firebase Services
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
